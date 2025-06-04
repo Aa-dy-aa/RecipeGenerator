a624840436c27b6aa105d765b73f172dcd2242f8
Aadyaa Sundriyal <sundriyalaadyaa77@gmail.com>
2025-06-04 11:06:06 +0530

    Lottie file added

--- app/create-recipe/[recipeId]/_components/RecipeBasicInfo.jsx
+++ app/create-recipe/[recipeId]/_components/RecipeBasicInfo.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Lottie from 'lottie-react';
 
 function RecipeBasicInfo({recipe}) {
+  const [animationData, setAnimationData] = useState(null);
+   useEffect(() => {
+    fetch('/placeholder.json') // ✅ correct public path
+      .then((res) => res.json())
+      .then(setAnimationData);
+  }, []);
   return (
     <div className='p-10 border rounded-xl shadow-sm mt-5'>
         <div className='grid grid-cols-1 md:grid-cols-2'>
@@ -8,7 +15,11 @@ function RecipeBasicInfo({recipe}) {
                 <h2 className='font-bold text-2xl'>{recipe?.recipeOutput?.recipeName}</h2>
                 <p className='text-xs text-gray-400 mt-3'>{recipe?.recipeOutput?.steps}</p>
             </div>
-            <div></div>
+            <div>
+               {animationData && (
+            <Lottie animationData={animationData} loop autoplay className='w-full rounded-xl' />
+          )}
+            </div>
         </div>
     </div>
   )
